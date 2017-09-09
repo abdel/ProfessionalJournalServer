c2fcf7f02df1d64a6c059a928bb8c91b01ed716d
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-09-09 12:59:59 +1000

    Add more fields to Author

--- tables/Author.js
+++ tables/Author.js
@@ -4,8 +4,12 @@ var table = azureMobileApps.table();
 
 // Defines the list of columns
 table.columns = {
-    "username": "string",
-    "password": "string"
+  "username": "string",
+  "password": "string",
+  "first_name": "string",
+  "last_name": "string",
+  "email": "string",
+  "date_of_birth": "date"
 };
 
 // Turns off dynamic schema
