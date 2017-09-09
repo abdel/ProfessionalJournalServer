22f3e77fed654291a77040444cf33f8b34ddf757
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-09-09 13:04:56 +1000

    Add journal table

---
+++ tables/Journal.js
@@ -0,0 +1,15 @@
+var azureMobileApps = require('azure-mobile-apps');
+
+var table = azureMobileApps.table();
+
+// Defines the list of columns
+table.columns = {
+  "title" "string",
+  "author_id": "string",
+  "description": "string"
+};
+
+// Turns off dynamic schema
+table.dynamicSchema = false;
+
+module.exports = table;
