7f9f67f465e7d9c6ac8ec41354c0269b65eaa035
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-09-22 16:49:19 +1000

    Fix fetch entries

--- api/entries.js
+++ api/entries.js
@@ -4,10 +4,8 @@ const wrap = require('co-express')
 const api = {
   get: wrap(function * (req, res, next) {
     const entries = yield req.azureMobile.data.execute({
-      sql: `SELECT * FROM Entry
-            WHERE author_id = @author_id AND journal_id = @journal_id;`,
+      sql: `SELECT * FROM Entry WHERE journal_id = @journal_id;`,
       parameters: [
-        { name: 'author_id', value: req.azureMobile.user.id },
         { name: 'journal_id', value: req.query.journal_id }
       ]
     })
