b78fe67a1678ead85d2dae512b116fc2a5de1694
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-10-08 15:16:56 +1100

    Add filter/search in entries endpoint

--- api/entries.js
+++ api/entries.js
@@ -3,11 +3,27 @@ const wrap = require('co-express')
 
 const api = {
   get: wrap(function * (req, res, next) {
+    let entryQuery = `SELECT * FROM Entry WHERE journal_id = @journal_id`
+    const entryParams = [ { name: 'journal_id', value: req.query.journal_id } ]
+
+    if (req.query.text !== null) {
+      entryQuery += ` AND title LIKE '%@title%'`
+      entryQuery.push({ name: 'title', value: req.query.text })
+    }
+
+    if (req.query.deleted === true) {
+      entryQuery += ` AND deleted = @deleted`
+      entryParams.push({ name: 'deleted', value: true })
+    }
+
+    if (req.query.hidden === true) {
+      entryQuery += ` AND hidden = @hidden`
+      entryParams.push({ name: 'hidden', value: true })
+    }
+
     const entries = yield req.azureMobile.data.execute({
-      sql: `SELECT * FROM Entry WHERE journal_id = @journal_id;`,
-      parameters: [
-        { name: 'journal_id', value: req.query.journal_id }
-      ]
+      sql: entryQuery,
+      parameters: entryParams
     })
 
     if (_.isEmpty(entries)) {
@@ -16,11 +32,12 @@ const api = {
     }
 
     for (let i = 0; i < entries.length; i++) {
+      const versionQuery = `SELECT * FROM EntryVersion WHERE id = @id`
+      const versionParams = [ { name: 'id', value: entries[i].entry_version_id } ]
+
       const entryVersion = yield req.azureMobile.data.execute({
-        sql: `SELECT * FROM EntryVersion WHERE id = @id;`,
-        parameters: [
-          { name: 'id', value: entries[i].entry_version_id }
-        ]
+        sql: versionQuery,
+        parameters: versionParams
       })
 
       entries[i].entry_version = entryVersion[0]
